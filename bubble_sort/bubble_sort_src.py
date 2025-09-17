93a197a36ed3fa69fc5bd363ccb29e1cedd87a48
Garry <github@garrynet.co.uk>
2025-09-17 23:39:38 +0100

    light touch-ups
     - added license
     - formatted files

--- bubble_sort/bubble_sort_src.py
+++ bubble_sort/bubble_sort_src.py
@@ -1,28 +1,39 @@
 # Made with ❤️ by Garry
 # 22/01/24
 
+
 def sort(array):
-    lengthOfData = len(array) # Determine the length of our array (Data)
+    lengthOfData = len(array)  # Determine the length of our array (Data)
 
-    for _pass in range(lengthOfData): # Iterate through our array, these will be our "passes"
+    for _pass in range(
+        lengthOfData
+    ):  # Iterate through our array, these will be our "passes"
 
-        for value in range(lengthOfData-_pass-1): # Iterate through the unsorted portion of the array, excluding the last _pass elements as they are already sorted.
+        for value in range(
+            lengthOfData - _pass - 1
+        ):  # Iterate through the unsorted portion of the array, excluding the last _pass elements as they are already sorted.
 
-            if array[value] > array[value+1]: # Compare two values and if the first one is larger than the second:
-                array[value], array[value+1] = array[value+1], array[value] # Swap the first and second values
+            if (
+                array[value] > array[value + 1]
+            ):  # Compare two values and if the first one is larger than the second:
+                array[value], array[value + 1] = (
+                    array[value + 1],
+                    array[value],
+                )  # Swap the first and second values
 
-            else: # If the first value is not larger than the second value:
-                pass # Do nothing
+            else:  # If the first value is not larger than the second value:
+                pass  # Do nothing
 
     return array
+
 
 if __name__ == "__main__":
     import random, time
 
-    data = [1,2,3,4,5,6,7,8,9,0] # Define our data set
-    random.shuffle(data) # Shuffle the data
+    data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]  # Define our data set
+    random.shuffle(data)  # Shuffle the data
 
-    print("Our unsorted data:", str(data)) # Print our original unsorted/shuffled data
+    print("Our unsorted data:", str(data))  # Print our original unsorted/shuffled data
 
     start = time.time()
 
@@ -30,5 +41,5 @@ if __name__ == "__main__":
 
     end = time.time()
 
-    print("\nOur sorted data:", str(data)) # Print out the final sorted data
-    print("Time taken:", str(end-start))
+    print("\nOur sorted data:", str(data))  # Print out the final sorted data
+    print("Time taken:", str(end - start))
--- run-all.py
+++ run-all.py
@@ -3,20 +3,22 @@
 
 import random, time
 
-from bubble_sort import bubble_sort_src 
+from bubble_sort import bubble_sort_src
 from merge_sort import merge_sort_src
 from insertion_sort import insertion_sort_src
 from quick_sort import quick_sort_src
+
 
 def define():
     arraySize = int(input("How large would you like the array? "))
     array = []
 
     for x in range(arraySize):
-        array.append(x+1)
+        array.append(x + 1)
 
     random.shuffle(array)
     return array
+
 
 def bubble(array):
     start = time.time()
@@ -24,37 +26,43 @@ def bubble(array):
     bubble_sort_src.sort(array)
 
     end = time.time()
-    timeTaken = end-start
+    timeTaken = end - start
 
     return timeTaken
+
+
 def merge(array):
     start = time.time()
 
     merge_sort_src.sort(array)
 
     end = time.time()
-    timeTaken = end-start
+    timeTaken = end - start
 
     return timeTaken
+
+
 def insertion(array):
     start = time.time()
 
     insertion_sort_src.sort(array)
 
     end = time.time()
-    timeTaken = end-start
+    timeTaken = end - start
 
     return timeTaken
+
 
 def quick(array):
     start = time.time()
 
-    quick_sort_src.sort(array, 0, len(array)-1)
+    quick_sort_src.sort(array, 0, len(array) - 1)
 
     end = time.time()
-    timeTaken = end-start
+    timeTaken = end - start
 
     return timeTaken
+
 
 if __name__ == "__main__":
     array = define()
